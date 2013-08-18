f1827d8cbf5612b00ca8be488d1f00de76f269b9
Pierre T <ptalbot@mopong.net>
2013-08-18 12:43:32 +0200

    Add a sql script to populate the database.

---
+++ data/umcd/database/populate_database.sql
@@ -0,0 +1,10 @@
+INSERT INTO addon_type (value,name) VALUES 
+('','CAMPAIGN_SP'), 
+('','CAMPAIGN_MP'),
+('','ERA'),
+('','MAP'),
+('','MAP_PACK'),
+('','RPG_SP'),
+('','RPG_MP'),
+('','MEDIA'),
+('','OTHER');
