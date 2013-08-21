21d3dc2b2a189aa25c1e880c8151cf39fcb8202d
Pierre T <ptalbot@mopong.net>
2013-08-21 09:48:28 +0200

    Add helper to send metadata header (here a config) over the network with a payload.

---
+++ src/umcd/protocol/header_data.cpp
@@ -0,0 +1,50 @@
+/*
+	Copyright (C) 2013 by Pierre Talbot <ptalbot@mopong.net>
+	Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY.
+
+	See the COPYING file for more details.
+*/
+
+#include "umcd/protocol/header_data.hpp"
+#include "config.hpp"
+#include <boost/make_shared.hpp>
+
+namespace umcd{
+
+boost::shared_ptr<sender_type> header_data::make_sender(socket_type& socket) const
+{
+	const_buffer_type buffers;
+	buffers.push_back(boost::asio::buffer(reinterpret_cast<const char*>(&payload_size), sizeof(payload_size)));
+	buffers.push_back(boost::asio::buffer(metadata));
+	return boost::make_shared<sender_type>(boost::ref(socket), boost::cref(shared_from_this()), boost::cref(buffers));
+}
+
+header_data& operator>>(const config& metadata, header_data& header)
+{
+	header.metadata = metadata.to_string();
+	header.payload_size = header.metadata.size();
+	return header;
+}
+
+config& operator<<(config& metadata, const header_data& header)
+{
+	std::stringstream header_stream(header.metadata);
+	::read(metadata, header_stream);
+	return metadata;
+}
+
+boost::shared_ptr<header_data::sender_type> make_header_sender(boost::asio::ip::tcp::socket& socket, const config& metadata)
+{
+	boost::shared_ptr<header_data> header = boost::make_shared<header_data>();
+	metadata >> *header;
+	return header->make_sender(socket);
+}
+
+} // namespace umcd
---
+++ src/umcd/protocol/header_data.hpp
@@ -0,0 +1,46 @@
+/*
+	Copyright (C) 2013 by Pierre Talbot <ptalbot@mopong.net>
+	Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY.
+
+	See the COPYING file for more details.
+*/
+
+#ifndef UMCD_HEADER_DATA_HPP
+#define UMCD_HEADER_DATA_HPP
+
+#include "umcd/server/data_sender.hpp"
+#include <vector>
+
+class config;
+
+namespace umcd{
+
+struct header_data : public boost::enable_shared_from_this<header_data>
+{
+	typedef std::vector<boost::asio::const_buffers_1> const_buffer_type;
+	typedef boost::asio::mutable_buffers_1 mutable_buffer_type;
+
+	typedef data_sender<header_data> sender_type;
+	typedef boost::asio::ip::tcp::socket socket_type;
+
+	boost::shared_ptr<sender_type> make_sender(socket_type& socket) const;
+	// boost::shared_ptr<receiver_type> make_receiver(socket_type& socket);
+
+	std::size_t payload_size;
+	std::string metadata;
+};
+
+header_data& operator>>(const config& metadata, header_data& header);
+config& operator<<(config& metadata, const header_data& header);
+boost::shared_ptr<header_data::sender_type> make_header_sender(boost::asio::ip::tcp::socket& socket, const config& metadata);
+
+} // namespace umcd
+
+#endif // UMCD_HEADER_DATA_HPP
