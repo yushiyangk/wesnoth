02632e53be2fe1a71e7c4463aff811d40575f035
Pierre T <ptalbot@mopong.net>
2013-08-24 18:27:38 +0200

    Remove all useless stuff in the protocol class. Most of things moved in the dispatcher or in dedicated files.

--- src/umcd/protocol/wml/protocol.cpp
+++ src/umcd/protocol/wml/protocol.cpp
@@ -24,72 +24,19 @@
 
 #include "umcd/protocol/header_data.hpp"
 
+#define FUNCTION_TRACER() UMCD_LOG_IP_FUNCTION_TRACER(*socket_)
+
 namespace umcd{
 
-std::size_t protocol::REQUEST_HEADER_MAX_SIZE = 8192;
-
-#define FUNCTION_TRACER() UMCD_LOG_IP_FUNCTION_TRACER(*socket_)
 
 protocol::protocol(io_service_type& io_service)
 : socket_(boost::make_shared<socket_type>(boost::ref(io_service)))
 {
 }
 
-config& protocol::get_reply()
-{
-	return reply_;
-}
-
-void protocol::load(const protocol_info& proto_info)
-{
-	REQUEST_HEADER_MAX_SIZE = proto_info.header_max_size();
-}
-
 protocol::socket_type& protocol::socket()
 {
 	return *socket_;
-}
-
-void protocol::complete_request(const boost::system::error_code& error, std::size_t)
-{
-	FUNCTION_TRACER();
-	if(error)
-	{
-		UMCD_LOG_IP(info, *socket_) << " -- unable to send data to the client (" << error.message() << "). Connection dropped.";
-		// TODO close socket.
-	}
-}
-
-void protocol::on_error(const boost::system::error_code& error)
-{
-	FUNCTION_TRACER();
-	if(error)
-	{
-		UMCD_LOG_IP(info, *socket_) << " -- unable to send data to the client (" << error.message() << "). Connection dropped.";
-		// TODO close socket.
-	}
-}
-
-void protocol::async_send_reply()
-{
-	FUNCTION_TRACER();
-
-	boost::shared_ptr<header_const_buffer::sender_type> sender = make_header_sender(socket_, reply_);
-	sender->on_event<transfer_error>(boost::bind(&protocol::on_error, shared_from_this(), boost::asio::placeholders::error));
-	sender->async_send();
-}
-
-void protocol::async_send_invalid_packet(const std::string &where, const std::exception& e)
-{
-	UMCD_LOG_IP(error, *socket_) << " -- invalid request at " << where << " (" << e.what() << ")";
-	async_send_error(socket_, make_error_condition(invalid_packet));
-}
-
-void protocol::async_send_invalid_packet(const std::string &where, const twml_exception& e)
-{
-	UMCD_LOG_IP(error, *socket_) << " -- invalid request at " << where 
-										<< " (user message=" << e.user_message << " ; dev message=" << e.dev_message << ")";
-	async_send_error(socket_, make_error_condition(invalid_packet));
 }
 
 void protocol::handle_request()
--- src/umcd/protocol/wml/protocol.hpp
+++ src/umcd/protocol/wml/protocol.hpp
@@ -28,47 +28,24 @@
 
 namespace umcd{
 
-class wml_request;
-class protocol_info;
-
 class protocol : 
 		public boost::enable_shared_from_this<protocol>
 	,	private boost::noncopyable
 {
 public:
-	static std::size_t REQUEST_HEADER_MAX_SIZE;
 	typedef boost::asio::ip::tcp::socket socket_type;
 	typedef boost::shared_ptr<socket_type> socket_ptr;
 	typedef boost::asio::io_service io_service_type;
 
-private:
-	typedef basic_umcd_action action_type;
-
 public:
-	static void load(const protocol_info& proto_info);
-
 	// This constructor is only called once in main, so the factory will be created once as well.
 	protocol(io_service_type& io_service);
 
 	void handle_request();
-	// Precondition: handle_request has been called and connection has been initialized.
-	void async_send_reply();
-
-	config& get_reply();
-
 	socket_type& socket();
 
 private:
-	void on_error(const boost::system::error_code& error);
-	void complete_request(const boost::system::error_code& error, std::size_t bytes_transferred);
-
-	void async_send_invalid_packet(const std::string &where, const std::exception& e);
-	void async_send_invalid_packet(const std::string &where, const twml_exception& e);
-
-private:
 	socket_ptr socket_;
-	config header_metadata_;
-	config reply_;
 };
 
 boost::shared_ptr<protocol> make_protocol(protocol::io_service_type& io_service);
