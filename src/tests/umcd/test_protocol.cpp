0f92b31edd8ec9dd69748f11966cdf3c57dedf32
Pierre T <ptalbot@mopong.net>
2013-09-10 23:21:48 +0200

    Update the test to use the send_receive_test facility, our first test consist of checking the result of an empty license request.

--- src/tests/umcd/test_protocol.cpp
+++ src/tests/umcd/test_protocol.cpp
@@ -12,18 +12,27 @@
    See the COPYING file for more details.
 */
 
-
+#include "tests/umcd/send_receive_test.hpp"
 #include "umcd/client/client.hpp"
 #include "umcd/protocol/make_header.hpp"
 #include "config.hpp"
-// #include "game_config.hpp"
+#include "game_config.hpp"
 #include "serialization/parser.hpp"
 
-#include <fstream>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <iostream>
 
 using namespace umcd;
 using namespace umcd::core;
+
+static void launch_test_suite(const boost::shared_ptr<boost::asio::ip::tcp::socket>& socket)
+{
+  boost::shared_ptr<send_receive_test> test_empty_license = boost::make_shared<send_receive_test>("empty license request", boost::cref(socket));
+  test_empty_license->async_launch(
+      game_config::path + "data/umcd/tests/request_license/request_license_empty_lang.cfg",
+      game_config::path + "data/umcd/protocol_schema/request_license_reply.cfg");
+}
 
 static void on_failure(const boost::system::error_code& error)
 {
@@ -33,13 +42,7 @@ static void on_failure(const boost::system::error_code& error)
 static void on_success(const boost::shared_ptr<boost::asio::ip::tcp::socket>& socket)
 {
   std::cout << "connected to " << socket->remote_endpoint() << std::endl;
-
-  std::ifstream request_file("../data/umcd/tests/request_license/request_license_empty_lang.cfg");
-  config request;
-  read(request, request_file);
-
-  boost::shared_ptr<header_const_buffer::sender_type> sender = make_header_sender(socket, request);
-  sender->async_send();
+  launch_test_suite(socket);
 }
 
 static void on_connect(const std::string& host_ip)
@@ -47,8 +50,17 @@ static void on_connect(const std::string& host_ip)
   std::cout << "Trying to connect at " << host_ip << std::endl;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+  if(argc != 2)
+  {
+    std::cerr << "usage: " << argv[0] << " wesnoth-dir" << std::endl;
+    return 1;
+  }
+  game_config::path = argv[1];
+  if(game_config::path.size() > 0 && *game_config::path.rbegin() != '/')
+    game_config::path += '/';
+
   boost::asio::io_service io_service;
   client c(io_service);
   c.on_event<try_connecting_with_ip>(on_connect);
@@ -56,5 +68,7 @@ int main()
   c.on_event<connection_failure>(on_failure);
   c.async_connect("localhost", "12522");
   io_service.run();
+
+  std::cout << "quit\n";
   return 0;
 }
