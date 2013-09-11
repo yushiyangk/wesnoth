88617b3f07d54a4d1b173212b284abce6782cbb6
Pierre T <ptalbot@mopong.net>
2013-09-11 15:56:49 +0200

    Add extension for the boost::uint16_t type, boost::array. Use OTL_PARANOID_EOF to catch potential mistakes.

--- src/umcd/otl/otl.hpp
+++ src/umcd/otl/otl.hpp
@@ -25,11 +25,50 @@
 #endif
 // Enable the use of the STL
 #define OTL_STL
+#define OTL_EXCEPTION_IS_DERIVED_FROM_STD_EXCEPTION
 
 #ifdef HAVE_CXX11
 	#define OTL_CPP_11_ON
 #endif
 
+// Extends the list of supported numeric data type.
+#include <boost/cstdint.hpp>
+#include <boost/lexical_cast.hpp>
+#include <cassert>
+
+// Extends for the boost::uint16_t.
+#define OTL_NUMERIC_TYPE_1 boost::uint16_t
+#define OTL_NUMERIC_TYPE_1_ID "uint16_t"
+#define OTL_NUMERIC_TYPE_1_STR_SIZE 7
+#define OTL_STR_TO_NUMERIC_TYPE_1(str,n)			\
+{																							\
+	n = boost::lexical_cast<boost::uint16_t> 		\
+		(str, OTL_NUMERIC_TYPE_1_STR_SIZE);				\
+}
+
+#define OTL_NUMERIC_TYPE_1_TO_STR(n, str)			\
+{																													\
+	std::string tmp = boost::lexical_cast<std::string>(n);	\
+	assert(tmp.size() < OTL_NUMERIC_TYPE_1_STR_SIZE);				\
+	std::copy(tmp.begin(), tmp.end(), str);									\
+}
+
+// Catch beyond-the-end-of-file read error.
+#define OTL_PARANOID_EOF
+
 #include "umcd/otl/otlv4.h"
+
+// Specializing stream operator >> and << for the boost array.
+template <size_t N>
+otl_stream& operator>>(otl_stream& s, boost::array<char, N>& a)
+{
+	return s >> a.elems;
+}
+
+template <size_t N>
+otl_stream& operator<<(otl_stream& s, boost::array<char, N>& a)
+{
+	return s << a.elems;
+}
 
 #endif // UMCD_OTL_HPP
