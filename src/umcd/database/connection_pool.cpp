31234459728c352a5bc73c543ecb0508a3db6d5c
Pierre T <ptalbot@mopong.net>
2013-09-13 09:24:36 +0200

    Add a connection pool class responsible to manage a pool of database connection.

---
+++ src/umcd/database/connection_pool.cpp
@@ -0,0 +1,135 @@
+/*
+	Copyright (C) 2013 by Pierre Talbot <ptalbot@mopong.net>
+	Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY.
+
+	See the COPYING file for more details.
+*/
+
+#include "umcd/database/connection_pool.hpp"
+#include "umcd/database/connection_instance.hpp"
+
+#include "umcd/boost/thread/lock_guard.hpp"
+#include <boost/make_shared.hpp>
+#include <boost/bind.hpp>
+
+namespace umcd{
+
+connection_pool::connection_pool(std::size_t pool_size
+		, boost::posix_time::time_duration wait_connection_timeout
+		, const std::string& db_connect_info)
+: pool_(pool_size)
+, connection_availability_(pool_size)
+, wait_connection_timeout_(wait_connection_timeout)
+, db_connect_info_(db_connect_info)
+{
+	for(std::size_t i = 0; i < pool_.size(); ++i)
+	{
+		connection_availability_.push_back(i);
+		pool_[i] = boost::make_shared<otl_connect>();
+	}
+}
+
+void connection_pool::dispatch_query(boost::asio::io_service &io_service
+		, query_function_type query
+		, timeout_function_type on_timeout)
+{
+	// Note: Be careful about this lock, it is not wrapped and, for example, an 
+	// exception could made the mutex pool_access_ locking forever.
+	// The code locked by this mutex is *exception free*.
+	pool_access_.lock();
+	// If no connection available.
+	if(connection_availability_.empty())
+	{
+		pool_access_.unlock();
+		// We add the task to the pending tasks queue with a timeout.
+		lock_guard<boost::mutex> pending_tasks_lock(pending_tasks_access_);
+		pending_tasks_.push_back(pending_task_type(
+			  query 
+			, boost::make_shared<boost::asio::deadline_timer>(boost::ref(io_service), wait_connection_timeout_))
+		);
+		pending_tasks_.back().timer->async_wait(
+			boost::bind(&connection_pool::pending_task_timeout, this, on_timeout, _1));
+	}
+	else
+	{
+		// Pick up a connection index and pop it from the vector.
+		std::size_t connection_index = connection_availability_.back();
+		connection_availability_.pop_back();
+		pool_access_.unlock();
+		// Dispatch the query request with the connection.
+		io_service.dispatch(boost::bind(query, 
+			boost::make_shared<connection_instance>(boost::ref(*this), connection_index))
+		);
+	}
+}
+
+void connection_pool::pending_task_timeout(
+			timeout_function_type on_timeout
+		, const boost::system::error_code& error)
+{
+	if(error != boost::asio::error::operation_aborted)
+	{
+		on_timeout();
+	}
+}
+
+otl_connect& connection_pool::get_instance(std::size_t instance_no)
+{
+	otl_connect& connection = *pool_[instance_no];
+	if(!connection.connected)
+		connection.rlogon(db_connect_info_.c_str());
+	return connection;
+}
+
+void connection_pool::notify_release(std::size_t instance_no)
+{
+	pending_tasks_access_.lock();
+	if(pending_tasks_.empty())
+	{
+		pending_tasks_access_.unlock();
+		lock_guard<boost::mutex> pool_lock(pool_access_);
+		connection_availability_.push_back(instance_no);
+	}
+	else
+	{
+		pending_task_type pending_task = pending_tasks_.front();
+		pending_tasks_.pop_front();
+		pending_tasks_access_.unlock();
+		boost::system::error_code cancel_error;
+		pending_task.timer->cancel(cancel_error);
+		if(!cancel_error)
+		{
+			// Why a post and not a dispatch?
+			// A dispatch means that the pending_task.task may be executed
+			// in this thread and delayed the current operation for non-expected
+			// work. Furthermore, pending_task.task can do the same and a infinite boucle
+			// can appear if all the pending_task.task are executed in cascad in this thread.
+			pending_task.timer->get_io_service().post(
+				boost::bind(pending_task.query,
+					boost::make_shared<connection_instance>(boost::ref(*this), instance_no))
+			);
+		}
+	}
+}
+
+connection_pool::~connection_pool()
+{
+	for(std::size_t i = 0; i < pool_.size(); ++i)
+		pool_[i]->logoff();
+}
+
+connection_pool::pending_task_type::pending_task_type(
+		query_function_type query
+	, const boost::shared_ptr<boost::asio::deadline_timer>& timer)
+: query(query)
+, timer(timer)
+{}
+
+} // namespace umcd
---
+++ src/umcd/database/connection_pool.hpp
@@ -0,0 +1,105 @@
+/*
+	Copyright (C) 2013 by Pierre Talbot <ptalbot@mopong.net>
+	Part of the Battle for Wesnoth Project http://www.wesnoth.org/
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY.
+
+	See the COPYING file for more details.
+*/
+
+#ifndef UMCD_DATABASE_CONNECTION_POOL_HPP
+#define UMCD_DATABASE_CONNECTION_POOL_HPP
+
+#include "umcd/otl/otl.hpp"
+
+#include "umcd/boost/thread/workaround.hpp"
+#include <boost/thread/mutex.hpp>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+#include <vector>
+#include <deque>
+#include <string>
+
+namespace umcd{
+
+class connection_instance;
+
+class connection_pool : public boost::noncopyable
+{
+public:
+	typedef boost::function<void()> timeout_function_type;
+	typedef boost::function<void(const boost::shared_ptr<connection_instance>&)> query_function_type;
+
+private:
+	friend class connection_instance;
+
+	otl_connect& get_instance(std::size_t instance_no);
+	void notify_release(std::size_t instance_no);
+	void pending_task_timeout(
+		  timeout_function_type on_timeout
+		, const boost::system::error_code& error);
+
+public:
+	connection_pool(std::size_t pool_size
+		, boost::posix_time::time_duration wait_connection_timeout
+		, const std::string& connect_str);
+
+	/** Asynchronous wait for an available connection if none are available.
+	* If one is available the dispatch method is called on io_service.
+	*/
+	void dispatch_query(boost::asio::io_service &io_service
+		, query_function_type query
+		, timeout_function_type on_timeout);
+
+	~connection_pool();
+
+private:
+	/** This pool contains all the connection and is responsible to maintain them alive
+	* (speaking about object lifetime and not about the connection itself).
+	* The otl_connect is inside a shared_ptr because it has no copy constructor and thus
+	* makes impossible the initialization of the vector.
+	*/
+	std::vector<boost::shared_ptr<otl_connect> > pool_;
+
+	/** We store the index of the available connections.
+	* The thread lock the pool_access_ mutex and pop the index in the back, if the 
+	* connection_availability_ is empty, then we must wait for a connection.
+	*/
+	boost::mutex pool_access_;
+	std::vector<std::size_t> connection_availability_;
+
+	/** The pending tasks queue is guarded by a mutex. It represents all the tasks
+	* waiting for a connection.
+	*/
+	struct pending_task_type
+	{
+		pending_task_type(query_function_type query
+			, const boost::shared_ptr<boost::asio::deadline_timer>& timer);
+
+		query_function_type query;
+		// It must be allocated on the heap in the case of the cancel operation occurring after
+		// the deadline_timer erased from the pending_tasks_.
+		boost::shared_ptr<boost::asio::deadline_timer> timer;
+	};
+
+	boost::mutex pending_tasks_access_;
+	std::deque<pending_task_type> pending_tasks_;
+
+	/** The maximum time we wait for a connection.
+	*/
+	boost::posix_time::time_duration wait_connection_timeout_;
+	/** String describing the database connection info as specified in the OTL doc.
+	*/
+	std::string db_connect_info_;
+};
+
+} // namespace umcd
+
+#endif // UMCD_DATABASE_CONNECTION_POOL_HPP
